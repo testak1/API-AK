7bf44b6c6f294cc9e64f11cf9712c0dbb64bd14e
testak1 <60112704+testak1@users.noreply.github.com>
2025-04-22 10:08:54 +0200

    Update brands.ts

--- frontend/pages/api/brands.ts
+++ frontend/pages/api/brands.ts
@@ -37,7 +37,7 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
                 origNm,
                 tunedNm,
                 price,
-                "description": descriptionRef->description
+                description
               }
             }
           }
