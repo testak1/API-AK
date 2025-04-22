f59ce93c85341906a54a7e64ba7bece40e58250f
testak1 <60112704+testak1@users.noreply.github.com>
2025-04-22 02:56:44 +0200

    Update App.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,6 @@
 // frontend/src/App.tsx
 import React, { useEffect, useState } from 'react'
-import { sanityClient } from '/lib/sanityClient'
+import { sanityClient } from 'lib/sanityClient'
 
 function App() {
   const [brands, setBrands] = useState<Brand[]>([])
