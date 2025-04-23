cb4fa76f0e4116de828b440a13426c5abdc3faaf
testak1 <60112704+testak1@users.noreply.github.com>
2025-04-23 11:29:12 +0200

    Update sanity.ts

--- frontend/lib/sanity.ts
+++ frontend/lib/sanity.ts
@@ -2,8 +2,8 @@ import { createClient } from '@sanity/client';
 import imageUrlBuilder from '@sanity/image-url';
 
 export const client = createClient({
-  projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
-  dataset: process.env.NEXT_PUBLIC_SANITY_DATASET,
+  projectId: 'wensahkh',
+  dataset: 'production',
   apiVersion: '2023-01-01',
   useCdn: true
 });
