cb2f4eeb6888b6eb32aa543ea1c1ba98d17cf590
testak1 <60112704+testak1@users.noreply.github.com>
2025-04-23 11:49:52 +0200

    Update index.tsx

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -181,8 +181,8 @@ export default function TuningViewer() {
 
 
 
-  return (
-    <div className="max-w-4xl mx-auto p-4 md:p-6 min-h-screen">
+
+
       {/* Header */}
       <div className="text-center mb-8">
         <h1 className="text-3xl font-bold bg-gradient-to-r from-blue-400 to-indigo-600 bg-clip-text text-transparent">
