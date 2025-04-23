f00c29441d04e70434720a5ddfb1188ea0219514
testak1 <60112704+testak1@users.noreply.github.com>
2025-04-23 12:46:18 +0200

    k

--- frontend/types/sanity.d.ts
+++ frontend/types/sanity.d.ts
@@ -1,7 +1,7 @@
 // types/sanity.d.ts
 import type { AktPlusOption, Brand, Engine, Model, Stage, Year } from '@/schemas';
 
-declare module '@/lib/sanity' {
+declare module '@./lib/sanity' {
   export const client: import('@sanity/client').SanityClient;
   export function urlFor(source: any): import('@sanity/image-url').ImageUrlBuilder;
   export function fetchSanityQuery<T = any>(
