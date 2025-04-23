32e9b440cab262513207cc9221cdaef44f0cd70f
testak1 <60112704+testak1@users.noreply.github.com>
2025-04-23 11:15:44 +0200

    Update index.tsx

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -3,7 +3,7 @@ import { useEffect, useState, useRef, useMemo } from 'react';
 import { Chart, CategoryScale, LinearScale, PointElement, LineElement, LineController } from 'chart.js';
 import { Line } from 'react-chartjs-2';
 import { PortableText } from '@portabletext/react';
-import { urlFor } from '@src/lib/sanity';
+import { urlFor } from '@./src/lib/sanity';
 
 Chart.register(CategoryScale, LinearScale, PointElement, LineElement, LineController);
 
