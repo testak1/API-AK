cd7d659fa2a1d874ac6ffacf22c4f2f954a88fbf
testak1 <60112704+testak1@users.noreply.github.com>
2025-04-25 00:23:50 +0200

    Update index.tsx

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -690,8 +690,7 @@ const renderStageDescription = (stage: Stage) => {
             );
           })}
         </div>
-      ) : (
-
+      ) : }
 
       <ContactModal
         isOpen={isContactModalOpen}
