284c8f47c7b9822f66c75d68880f3c667970ff96
testak1 <60112704+testak1@users.noreply.github.com>
2025-04-25 00:33:04 +0200

    Update index.tsx

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -690,7 +690,13 @@ const renderStageDescription = (stage: Stage) => {
             );
           })}
         </div>
-
+      ) : (
+        <div className="text-center py-12 bg-gray-800 rounded-xl">
+          <p className="text-white">
+EXTRA INFO RUTA KANSKE?
+          </p>
+        </div>
+      )}
 
       <ContactModal
         isOpen={isContactModalOpen}
