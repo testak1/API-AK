b7f3c8ff1e7609fed08f3e177c81ddb29bc90b8d
testak1 <60112704+testak1@users.noreply.github.com>
2025-04-25 19:39:51 +0200

    Update years.ts

--- frontend/pages/api/years.ts
+++ frontend/pages/api/years.ts
@@ -1,5 +1,5 @@
-import { NextApiRequest, NextApiResponse } from 'next';
-import { client } from '@/lib/sanity';
+import { NextApiRequest, NextApiResponse } from '
+import client from '@/lib/sanity';
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
   const { brand, model } = req.query;
