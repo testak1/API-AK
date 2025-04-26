1578cac41e66d9f0c73231ea503f6597c17803b9
testak1 <60112704+testak1@users.noreply.github.com>
2025-04-26 15:33:28 +0200

    Update index.tsx

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -539,7 +539,7 @@ const renderStageDescription = (stage: Stage) => {
           </div>
 
           {/* ORG NM / Max NM */}
-          <div className="px-3 py-2 rounded text-sm">
+          <div className="py-2 rounded text-sm">
             <p className="text-blue-600">- -</p>
             <p className="text-white">ORG NM: {stage.origNm}</p>
             <p className="text-blue-600">⸺</p>
