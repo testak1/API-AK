0dbad8eef7328fd478be3d07c57d3d201e1fcadd
testak1 <60112704+testak1@users.noreply.github.com>
2025-04-26 02:02:48 +0200

    Update send-contact.ts

--- frontend/pages/api/send-contact.ts
+++ frontend/pages/api/send-contact.ts
@@ -15,8 +15,8 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
     return res.status(400).json({ error: 'Alla fält måste fyllas i.' });
   }
 
-  let recipientEmail = ";
-  switch (branch.toLowerCase()) {
+  let recipientEmail = '';
+  switch (branch) {
     case 'TEST-AK':
       recipientEmail = 'info@aktuning.se';
       break;
