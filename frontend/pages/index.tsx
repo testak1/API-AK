c8d014bd9235544110d7d0b2305539603a41c435
testak1 <60112704+testak1@users.noreply.github.com>
2025-04-26 18:18:16 +0200

    Update index.tsx

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -531,9 +531,9 @@ const renderStageDescription = (stage: Stage) => {
 
       <div className="h-96 bg-gray-900 rounded-lg p-4 relative">
         {/* Split the spec boxes */}
-        <div className="absolute flex flex-row justify-between top-4 left-0 right-0 px-14">
+        <div className="absolute flex flex-row justify-between top-4 left-0 right-0 px-16">
           {/* ORG HK / Max HK */}
-          <div className="bg-gray-900 px-4 py-3 rounded text-sm text-white flex flex-col items-start w-30">
+          <div className="bg-gray-900 px-4 py-3 rounded text-xs text-white flex flex-col items-start w-30">
             <p className="text-red-600">- -</p>
             <p className="text-white">org hk: {stage.origHk}</p>
             <p className="text-red-600">⸺</p>
@@ -541,7 +541,7 @@ const renderStageDescription = (stage: Stage) => {
           </div>
 
           {/* ORG NM / Max NM */}
-          <div className="bg-gray-900 px-4 py-3 rounded text-sm text-white flex flex-col items-start w-30">
+          <div className="bg-gray-900 px-4 py-3 rounded text-xs text-white flex flex-col items-start w-30">
             <p className="text-white">- -</p>
             <p className="text-white">org nm: {stage.origNm}</p>
             <p className="text-white">⸺</p>
