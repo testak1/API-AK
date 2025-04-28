7d29fc011f4e330d20ee48a1bb710108ae89a482
testak1 <60112704+testak1@users.noreply.github.com>
2025-04-28 03:53:03 +0200

    Update page.tsx

--- frontend/pages/[brand]/[model]/[year]/[engine]/page.tsx
+++ frontend/pages/[brand]/[model]/[year]/[engine]/page.tsx
@@ -2,7 +2,7 @@
 
 import { notFound } from "next/navigation";
 import { PortableText } from "@portabletext/react";
-import sanityClient from "@/lib/sanityClient";
+import client from "@/lib/sanity";
 import { allBrandsQuery } from "@/lib/queries";
 
 interface Props {
