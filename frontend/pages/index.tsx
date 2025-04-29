4e1913116158ea124ef3f63c2311d18bc5dbcb80
testak1 <60112704+testak1@users.noreply.github.com>
2025-04-29 16:49:50 +0200

    Update index.tsx

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -464,7 +464,7 @@ export default function TuningViewer() {
 
   return (
     <div className="max-w-5xl mx-auto p-4 md:p-8">
-      <div className="flex items-center mb-8">
+      <div className="flex items-center mb-4">
         <img
           src="/ak-logo-svart.png"
           alt="AK-TUNING"
@@ -473,10 +473,9 @@ export default function TuningViewer() {
         />
       </div>
 
-
       <div className="mb-4">
         <p className="text-black text-center text-lg font-semibold">
-           Välj din bil nedan för att se vad vi kan erbjuda
+          Välj din bil nedan för att se vad vi kan erbjuda
         </p>
       </div>
       <div className="grid grid-cols-1 md:grid-cols-4 gap-3 mb-8">
