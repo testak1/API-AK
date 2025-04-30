8d94ff79f5a09db84f24066484c945727bc0a5db
testak1 <60112704+testak1@users.noreply.github.com>
2025-04-30 14:51:42 +0200

    Update queries.ts

--- frontend/src/lib/queries.ts
+++ frontend/src/lib/queries.ts
@@ -17,33 +17,38 @@ export const brandsLightQuery = `
 }
 `;
 
-export const engineByParamsQuery = (brand: string, model: string, year: string, engine: string) => `
-  *[_type == "brand" && slug.current == "${brand}"][0]{
-    name,
-    "models": models[name == "${model}"][0]{
+export const engineByParamsQuery = `
+  query EngineByParams($brand: String!, $model: String!, $year: String!, $engine: String!) {
+    "brandData": *[_type == "brand" && slug.current == $brand][0]{
       name,
-      "years": years[range == "${year}"][0]{
-        range,
-        "engines": engines[label == "${engine}"][0]{
-          label,
-          fuel,
-          "stages": stages[]{
-            name,
-            origHk,
-            tunedHk,
-            origNm,
-            tunedNm,
-            price,
-            description,
-            descriptionRef->{
-              _id,
-              stageName,
-              description
+      "models": models[]{
+        name,
+        "years": years[]{
+          range,
+          "engines": engines[]{
+            label,
+            fuel,
+            "stages": stages[]{
+              name,
+              origHk,
+              tunedHk,
+              origNm,
+              tunedNm,
+              price,
+              description,
+              descriptionRef->{
+                description
+              }
             }
           }
         }
       }
     }
+  } | {
+    "brandData": brandData,
+    "modelData": brandData.models[name match $model][0],
+    "yearData": brandData.models[name match $model][0].years[range match $year][0],
+    "engineData": brandData.models[name match $model][0].years[range match $year][0].engines[label match $engine][0]
   }
 `;
 
