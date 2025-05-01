59e1f468d1d307bf0f366b2fb3060e9c3769d240
testak1 <60112704+testak1@users.noreply.github.com>
2025-05-01 18:06:57 +0200

    Update index.tsx

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -513,7 +513,7 @@ export default function TuningViewer() {
     );
   };
   return (
-    <div className="max-w-[600px] mx-auto px-2 sm:px-4 md:px-8">
+    <div className="max-w-5xl mx-auto p-4 md:p-8">
       <div className="flex items-center mb-4">
         <img
           src="/ak-logo-svart.png"
