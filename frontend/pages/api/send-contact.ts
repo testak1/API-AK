6d8b27aec7da61ff9373b4ffa9fb02dedc9c4927
testak1 <60112704+testak1@users.noreply.github.com>
2025-05-05 21:52:25 +0200

    Update send-contact.ts

--- frontend/pages/api/send-contact.ts
+++ frontend/pages/api/send-contact.ts
@@ -37,7 +37,7 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
 
   try {
     const response = await resend.emails.send({
-      from: 'prestanda@aktuning.se',
+      from: 'MOTOROPTIMERING',
       to: recipientEmail,
       subject: `FÖRFRÅGAN - ${vehicle?.brand || ''} ${vehicle?.model || ''} ${vehicle?.year || ''} ${vehicle?.engine || ''} | ${(stage || '-').toUpperCase()}`,      
       replyTo: email,
