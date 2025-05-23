fed6837a029f8a66b512393f935aeb306cc0c52d
testak1 <60112704+testak1@users.noreply.github.com>
2025-05-23 20:49:07 +0200

    Update reseller-config.ts

--- frontend/pages/api/reseller-config.ts
+++ frontend/pages/api/reseller-config.ts
@@ -12,5 +12,7 @@ export default async function handler(req, res) {
 
   res.status(200).json({
     logo: result?.logo ?? null,
+    currency: result?.currency ?? "SEK",
+    language: result?.language ?? "sv",
   });
 }
