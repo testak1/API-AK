2f4ef78b7bbb9c85255561a2670ee1cab6d4907f
testak1 <60112704+testak1@users.noreply.github.com>
2025-05-28 12:44:25 +0200

    333

--- frontend/pages/api/reseller-config.ts
+++ frontend/pages/api/reseller-config.ts
@@ -31,7 +31,7 @@ export default async function handler(req, res) {
           showBrandLogo,
           showStageLogo,
           showDynoChart
-        }
+        },
         subscription
       }`,
       { resellerId },
