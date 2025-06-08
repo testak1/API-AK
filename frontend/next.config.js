557437337de1c1dd34349c4d3fffd54b10f3d35d
testak1 <60112704+testak1@users.noreply.github.com>
2025-06-08 19:21:59 +0200

    Update next.config.js

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -3,105 +3,18 @@ const nextConfig = {
   images: {
     remotePatterns: [
       {
-        protocol: "https",
-        hostname: "cdn.sanity.io",
-      },
-      {
-        protocol: "https",
-        hostname: "tuning.aktuning.se",
+        protocol: 'https',
+        hostname: 'cdn.sanity.io',
+ 
       },
     ],
-    minimumCacheTTL: 60,
-    formats: ["image/webp"],
-    deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
-    imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
   },
-
-  async redirects() {
-    return [
-      {
-        source: "/:path*",
-        has: [
-          {
-            type: "host",
-            value: "www.tuning.aktuning.se",
-          },
-        ],
-        destination: "https://tuning.aktuning.se/:path*",
-        permanent: true,
-      },
-    ];
-  },
-
   reactStrictMode: true,
-  swcMinify: true,
-  compress: true,
-  productionBrowserSourceMaps: false,
   env: {
     SANITY_PROJECT_ID: "dinSanityProjectId",
     SANITY_DATASET: "production",
     NEXT_PUBLIC_API_BASE: "https://api.aktuning.se",
   },
-
-  async headers() {
-    return [
-      {
-        source: "/(.*)",
-        headers: [
-          {
-            key: "X-DNS-Prefetch-Control",
-            value: "on",
-          },
-          {
-            key: "Strict-Transport-Security",
-            value: "max-age=63072000; includeSubDomains; preload",
-          },
-          {
-            key: "X-XSS-Protection",
-            value: "1; mode=block",
-          },
-          {
-            key: "X-Content-Type-Options",
-            value: "nosniff",
-          },
-          {
-            key: "Referrer-Policy",
-            value: "origin-when-cross-origin",
-          },
-        ],
-      },
-
-      {
-        source: "/sitemap.xml",
-        headers: [
-          {
-            key: "Cache-Control",
-            value: "no-cache, no-store, must-revalidate",
-          },
-        ],
-      },
-      {
-        source: "/_next/static/(.*)",
-        headers: [
-          {
-            key: "Cache-Control",
-            value: "public, max-age=31536000, immutable",
-          },
-        ],
-      },
-      {
-        source: "/static/(.*)",
-        headers: [
-          {
-            key: "Cache-Control",
-            value: "public, max-age=31536000, immutable",
-          },
-        ],
-      },
-    ];
-  },
-
-  experimental: {},
 };
 
 module.exports = nextConfig;
