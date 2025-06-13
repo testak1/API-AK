7faacb6b53a9a8105f54e22b098bcea9b06d7fef
testak1 <60112704+testak1@users.noreply.github.com>
2025-06-13 17:47:45 +0200

    Update next.config.js

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -5,11 +5,10 @@ const nextConfig = {
       {
         protocol: 'https',
         hostname: 'cdn.sanity.io',
- 
       },
-       {
-        protocol: "https",
-        hostname: "tuning.aktuning.se",
+      {
+        protocol: 'https',
+        hostname: 'tuning.aktuning.se',
       },
     ],
   },
@@ -18,6 +17,27 @@ const nextConfig = {
     SANITY_PROJECT_ID: "dinSanityProjectId",
     SANITY_DATASET: "production",
     NEXT_PUBLIC_API_BASE: "https://api.aktuning.se",
+  },
+  async headers() {
+    return [
+      {
+        source: '/sitemap-1.xml',
+        headers: [
+          {
+            key: 'Cache-Control',
+            value: 'no-cache, no-store, must-revalidate',
+          },
+          {
+            key: 'Pragma',
+            value: 'no-cache',
+          },
+          {
+            key: 'Expires',
+            value: '0',
+          },
+        ],
+      },
+    ];
   },
 };
 
