82f600509bcfe5aeb5bccdb1b6e6c4225df481a8
testak1 <60112704+testak1@users.noreply.github.com>
2025-07-31 00:54:30 +0200

    Update RegnrSearch.tsx

--- frontend/components/RegnrSearch.tsx
+++ frontend/components/RegnrSearch.tsx
@@ -113,7 +113,7 @@ export default function RegnrSearch({ onVehicleFound, onError }: { onVehicleFoun
             <path strokeLinecap="round" strokeLinejoin="round" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" />
           </svg>
           <span className="font-semibold text-white">
-            REGNR "(under utveckling!)"
+            REGNR (BETA)
           </span>
         </div>
         <svg className="w-5 h-5 text-gray-400 transform transition-transform group-open:rotate-180" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
