f3576097d7c620e1fa6a17aa6888568dca362ecc
testak1 <60112704+testak1@users.noreply.github.com>
2025-09-20 20:30:37 +0200

    Update [engine].tsx

--- frontend/pages/[brand]/[model]/[year]/[engine].tsx
+++ frontend/pages/[brand]/[model]/[year]/[engine].tsx
@@ -880,10 +880,10 @@ export default function EnginePage({
                 {yearData.range} – {engineData.label}
               </h1>
               <Link
-                href={`/${getSlugValue(brandData.slug, brandData.name)}/${getSlugValue(modelData.slug, modelData.name)}/${getSlugValue(yearData.slug, yearData.range)}`}
+                href={`/${brandSlug}/${modelSlug}/${yearSlug}`}
                 className="text-sm text-orange-500 hover:underline"
               >
-                ← {translate(currentLanguage, "BACKTO")} {yearData.range}
+                ← Tillbaka till {yearData.range}
               </Link>
             </div>
           )}
