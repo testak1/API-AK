951e5887ee53098b00e8fe1510e076db035d3f90
testak1 <60112704+testak1@users.noreply.github.com>
2025-10-25 21:03:58 +0200

    Update sanity.server.ts

--- frontend/lib/sanity.server.ts
+++ frontend/lib/sanity.server.ts
@@ -3,7 +3,7 @@ import {createClient} from "@sanity/client";
 export const sanityClient = createClient({
   projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID || "wensahkh",
   dataset: process.env.NEXT_PUBLIC_SANITY_DATASET || "production",
-  apiVersion: "2025-10-01",
+  apiVersion: "2025-04-23",
   useCdn: false,
   token: process.env.SANITY_WRITE_TOKEN,
 });
